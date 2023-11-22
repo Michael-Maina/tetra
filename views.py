f4dc974ea97a9de1018f53a2318a36cf243c2031
Linda-Njau <lindanjau21@gmail.com>
2023-11-22 12:44:49 +0300

    Logic to automate cleaning of extracted data storage dictionary

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
 import os
+import time
+import threading
 from flask import Flask, request, jsonify, Response, Blueprint
 import tempfile
 from transcribe_audio import transcribe_audio
@@ -21,7 +23,13 @@ def handle_audio():
         
         extracted_data = extract_entities(transcription_text)
         data_key = str(hash(transcription_text))
-        extracted_data_storage[data_key] = extracted_data
+        current_time = time.time()
+        
+        extracted_data_storage[data_key] = {
+            "extracted_data":extracted_data,
+            "timestamp": current_time
+            }
+        
         return jsonify({
             "transcription_text": transcription_text,
             "data_key": data_key, 
@@ -34,6 +42,22 @@ def handle_audio():
 @audio.route('/extracted_data/<data_key', methods=['GET'])
 def get_extracted_data(data_key):
     if data_key in extracted_data_storage:
-        return (extracted_data_storage[data_key]), 200
+        return (extracted_data_storage[data_key]["extracted_data"]), 200
     else:
         return jsonify({"error": "Data not found"}), 404
+
+def cleanup_extracted_data():
+    expiration_threshold = 3600
+    current_time = time.time()
+    keys_to_delete = [key for key, value in extracted_data_storage.items()
+                      if current_time - value["timestamp"] > expiration_threshold]
+    for key in keys_to_delete:
+        del extracted_data_storage[key]
+
+def start_cleanup_thread():
+    def cleanup_loop():
+        while True:
+            cleanup_extracted_data()
+            time.sleep(3600)
+    thread = threading.Thread(target=cleanup_loop)
+    thread.start()
