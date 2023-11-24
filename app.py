028fe27af3fe344843355acad410c5121aefb21e
Michael-Maina <michaelmaina02@gmail.com>
2023-11-24 09:50:36 +0300

    Removed app variable duplication

--- app.py
+++ app.py
@@ -21,6 +21,16 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 
+# Scheduler initialization
+scheduler = APScheduler()
+scheduler.init_app(app)
+app.scheduler = scheduler
+
+scheduler.add_job(id='Scheduled Cleanup', func=cleanup_extracted_data,
+                    trigger='interval', seconds=3600)
+
+scheduler.start()
+
 # Flask-Login init
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -61,24 +71,7 @@ def load_user(user_id):
     return User.objects.get(id=user_id)
 
 
-def create_app():
-    load_dotenv()
-    app = Flask(__name__)
-    scheduler = APScheduler()
-    scheduler.init_app(app)
-    app.scheduler = scheduler
-
-    scheduler.add_job(id='Scheduled Cleanup', func=cleanup_extracted_data,
-                      trigger='interval', seconds=3600)
-
-    scheduler.start()
-
-    app.register_blueprint(audio)
-    return app
-
 if __name__ == '__main__':
-    app = create_app()
-
     API_HOST = getenv('API_HOST')
     API_PORT = getenv('API_PORT')
 
